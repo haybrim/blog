b0b8460aa642557b368d3f7da4032a4320a00445
Hayden <hb168698@umconnect.umt.edu>
2022-10-04 11:37:52 -0600

    new additions

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from .views import BlogListView, BlogDetailView
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("", BlogListView.as_view(), name="home"),
 ]
